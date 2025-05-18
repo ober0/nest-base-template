7bdfe65e2ae50daf1650e17142cf9e7a6d6fa2c9
Ruslan <onishruslan@yandex.ru>
2025-05-18 15:45:57 +0300

    обновил пермишены

--- src/common/constants/permission.enum.ts
+++ src/common/constants/permission.enum.ts
@@ -1,27 +1,29 @@
 export enum PermissionEnum {
-    UserCreate = 'user-create',
-    UserGet = 'user-get',
-    UserGetAll = 'user-get-all',
-    UserUpdate = 'user-update',
-    UserDelete = 'user-delete',
+    UserCreate = 'user:create',
+    UserGet = 'user:get',
+    UserGetAll = 'user:get-all',
+    UserUpdate = 'user:update',
+    UserDelete = 'user:delete',
 
-    RoleCreate = 'role-create',
-    RoleGet = 'role-get',
-    RoleGetAll = 'role-get-all',
-    RoleUpdate = 'role-update',
-    RoleDelete = 'role-delete',
+    RoleCreate = 'role:create',
+    RoleGet = 'role:get',
+    RoleGetAll = 'role:get-all',
+    RoleUpdate = 'role:update',
+    RoleDelete = 'role:delete',
 
-    RolePermissionCreate = 'role-permission-create',
-    RolePermissionGet = 'role-permission-get',
-    RolePermissionGetAll = 'role-permission-get-all',
-    RolePermissionUpdate = 'role-permission-update',
-    RolePermissionDelete = 'role-permission-delete',
+    RolePermissionCreate = 'role-permission:create',
+    RolePermissionGet = 'role-permission:get',
+    RolePermissionGetAll = 'role-permission:get-all',
+    RolePermissionUpdate = 'role-permission:update',
+    RolePermissionDelete = 'role-permission:delete',
 
-    PermissionCreate = 'plan-create',
-    PermissionGet = 'plan-get',
-    PermissionGetAll = 'plan-get-all',
-    PermissionUpdate = 'plan-update',
-    PermissionDelete = 'plan-delete'
+    PermissionCreate = 'permission:create',
+    PermissionGet = 'permission:get',
+    PermissionGetAll = 'permission:get-all',
+    PermissionUpdate = 'permission:update',
+    PermissionDelete = 'permission:delete',
+
+    LoginHistorySearch = 'login-history:search'
 }
 
 export const PermissionDescriptions: Record<any, string> = {
@@ -44,5 +46,7 @@ export const PermissionDescriptions: Record<any, string> = {
     [PermissionEnum.PermissionGet]: 'Получение разрешений',
     [PermissionEnum.PermissionGetAll]: 'Получение всех разрешений',
     [PermissionEnum.PermissionUpdate]: 'Обновление разрешений',
-    [PermissionEnum.PermissionDelete]: 'Удаление разрешений'
+    [PermissionEnum.PermissionDelete]: 'Удаление разрешений',
+
+    [PermissionEnum.LoginHistorySearch]: 'Поиск по журналу входов'
 }
